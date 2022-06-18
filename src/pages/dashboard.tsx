262f49a52f278594a77be8a86f32de72e51cba28
Thyago R <thyagoribeiro608@gmail.com>
2022-06-18 14:59:53 -0400

    Created Component Dashboard and charts ts

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,13 @@
 import Head from "next/head";
+import dynamic from "next/dynamic";
 import { Header } from "components/Header";
-import { Flex } from "@chakra-ui/react";
+import { Sidebar } from "components/Sidebar";
+import { Box, Flex, SimpleGrid, Text } from "@chakra-ui/react";
+import { options, series } from "lib/charts";
+
+const Charts = dynamic(() => import("react-apexcharts"), {
+  ssr: false,
+});
 
 export default function Dashboard() {
   return (
@@ -8,12 +15,34 @@ export default function Dashboard() {
       <Head>
         <title>Dashboard | Dashgo</title>
       </Head>
-      <Flex>
+      <Flex direction="column" h="100vh">
         <Header />
-        <Flex
-            
-        >
-
+        <Flex w="100%" my="6" maxWidth={1200} mx="auto" px="6">
+          <Sidebar />
+          <SimpleGrid flex="1" gap="4" minChildWidth="320px">
+            <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+              <Text fontSize="lg" mb="4">
+                Inscritos da semana
+              </Text>
+              <Charts
+                options={options}
+                series={series}
+                type="area"
+                height={160}
+              />
+            </Box>
+            <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+              <Text fontSize="lg" mb="4">
+                Taxa de abertura
+              </Text>
+              <Charts
+                options={options}
+                series={series}
+                type="area"
+                height={160}
+              />
+            </Box>
+          </SimpleGrid>
         </Flex>
       </Flex>
     </>
