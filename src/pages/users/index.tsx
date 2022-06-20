c04998248b8c7308417704bda99f5b4cfc970246
Thyago R <thyagoribeiro608@gmail.com>
2022-06-20 19:43:39 -0400

    Create Page Users

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,91 @@
+import Head from "next/head";
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import { Sidebar } from "components/Sidebar";
+import { Header } from "components/Header";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
+
+export default function UserList() {
+  return (
+    <>
+      <Head>
+        <title>Listagem de Usuários | Dashgo</title>
+      </Head>
+      <Box>
+        <Header />
+        <Flex w="100%" my="6" maxWidth={1200} mx="auto" px="6">
+          <Sidebar />
+          <Box flex="1" borderRadius={6} bg="gray.800" p="8">
+            <Flex mb="8" justify="space-between" align="center">
+              <Heading size="lg" fontWeight="normal">
+                Usuários
+              </Heading>
+              <Button
+                as="a"
+                size="sm"
+                fontSize="sm"
+                colorScheme="pink"
+                leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+                cursor="pointer"
+              >
+                Criar novo
+              </Button>
+            </Flex>
+
+            <Table colorScheme="whiteAlpha">
+              <Thead>
+                <Tr>
+                  <Th px="6" color="gray.300" width="8">
+                    <Checkbox colorScheme="pink" />
+                  </Th>
+                  <Th>Usuário</Th>
+                  <Th>Data de Cadastro</Th>
+                  <Th width="8"></Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                <Tr>
+                  <Td px="6">
+                    <Checkbox colorScheme="pink" />
+                  </Td>
+                  <Td>
+                    <Box>
+                      <Text fontWeight="bold">Thyago Ribeiro</Text>
+                      <Text fontSize="sm" color="gray.300">
+                        thyagoribeiro608@gmail.com
+                      </Text>
+                    </Box>
+                  </Td>
+                  <Td>20 de Junho, 2022</Td>
+                  <Td>
+                    <Button
+                      size="sm"
+                      fontSize="sm"
+                      colorScheme="purple"
+                      cursor="pointer"
+                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                      iconSpacing={0}
+                    />
+                  </Td>
+                </Tr>
+              </Tbody>
+            </Table>
+          </Box>
+        </Flex>
+      </Box>
+    </>
+  );
+}
