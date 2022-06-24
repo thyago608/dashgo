5977afa405e6b27c5615175c6b67852113583377
Thyago R <thyagoribeiro608@gmail.com>
2022-06-23 20:30:57 -0400

    Updated PaginationItem

--- src/components/Pagination/PaginationItem.tsx
+++ src/components/Pagination/PaginationItem.tsx
@@ -1,41 +1,19 @@
-import { Button } from "@chakra-ui/react";
+import { Button, ButtonProps } from "@chakra-ui/react";
 
 interface PaginationItemProps {
   isCurrent?: boolean;
   number: number;
 }
 
-export function PaginationItem({
-  isCurrent = false,
-  number,
-}: PaginationItemProps) {
-  if (isCurrent) {
-    <Button
-      size="xs"
-      fontSize="xs"
-      width="4"
-      colorScheme="pink"
-      disabled
-      _disabled={{
-        bg: "pink.500",
-        cursor: "default",
-      }}
-    >
-      {number}
-    </Button>;
-  }
+export function PaginationItem({ isCurrent = false, number }: PaginationItemProps) {
+  
+    const buttonStyle: ButtonProps = isCurrent ? 
+        { colorScheme: "pink", disabled: true, _disabled: { bg: "pink.500", cursor: "default" }}: 
+        { bg: "gray.700", _hover: { bg: "gray.500" }};
 
-  return (
-    <Button
-      size="xs"
-      fontSize="xs"
-      width="4"
-      bg="gray.700"
-      _hover={{
-        bg: "gray.500",
-      }}
-    >
-      {number}
-    </Button>
-  );
+    return (
+        <Button size="xs" fontSize="xs" width="4" {...buttonStyle}>
+            {number}
+        </Button>
+    );
 }
