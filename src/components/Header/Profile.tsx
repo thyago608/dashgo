379f993a7add505ee78877a72c4fe91268931c45
Thyago R <thyagoribeiro608@gmail.com>
2022-06-24 07:13:48 -0400

    Added Responsive in Header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,19 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
+interface ProfileProps {
+  showProfileData?: boolean;
+}
 
-export function Profile() {
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Thyago Ribeiro</Text>
-        <Text color="gray.300" fontSize="small">
-          thyagoribeiro608@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Thyago Ribeiro</Text>
+          <Text color="gray.300" fontSize="small">
+            thyagoribeiro608@gmail.com
+          </Text>
+        </Box>
+      )}
       <Avatar
         size="md"
         name="Thyago Ribeiro"
